1d483584e95c7a0d5e1e074f996142e8d97eca8f
Jordan Smith <jordan.m.smith721@gmail.com>
2019-02-13 12:59:02 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jsmit
+ * Date: 2/13/2019
+ * Time: 9:56 AM
+ */
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once 'vendor/autoload.php';
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+//Define a default route
+$f3->route('GET /', function ()
+{
+    echo "<h1>Student Page</h1>";
+});
+
+//Run fat free
+$f3->run();
